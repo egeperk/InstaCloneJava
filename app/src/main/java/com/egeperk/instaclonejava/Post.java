5d0d52cf3413aae8f018cd1158d7c181f5615f39
egeperk <98737865+egeperk@users.noreply.github.com>
2022-01-31 13:04:20 +0300

    Clone application

---
+++ app/src/main/java/com/egeperk/instaclonejava/Post.java
@@ -0,0 +1,14 @@
+package com.egeperk.instaclonejava;
+
+public class Post {
+
+public String email;
+public String comment;
+public String downloadUrl;
+
+    public Post(String email, String comment, String downloadurl) {
+        this.email = email;
+        this.comment = comment;
+        this.downloadUrl = downloadurl;
+    }
+}
